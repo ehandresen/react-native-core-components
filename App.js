cba621ac8eb16fabec12e471ce33a90dd154a569
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-30 18:43:52 +0200

    ActivityIndicator component

--- App.js
+++ App.js
@@ -1,30 +1,9 @@
-import { View, StatusBar, Button } from 'react-native';
-import { useState } from 'react';
-
-// using require (commonJS) is preferred when dealing with static assets like images in react-native
-const logoImg = require('./assets/adaptive-icon.png');
+import { View, ActivityIndicator } from 'react-native';
 
 export default function App() {
-  const [showStatusBar, setShowStatusBar] = useState(false);
-  const [color, setColor] = useState('dark-content');
-
-  const changeColor = () => {
-    if (color === 'dark-content') {
-      setColor('light-content');
-      return;
-    }
-
-    setColor('dark-content');
-  };
-
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <StatusBar barStyle={color} hidden={showStatusBar} />
-      <Button title="change color" onPress={changeColor} />
-      <Button
-        title="show/hide statusbar"
-        onPress={() => setShowStatusBar(!showStatusBar)}
-      />
+      <ActivityIndicator size={'large'} color={'red'} animating={true} />
     </View>
   );
 }
