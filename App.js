d3302a895e354b62f2f2d1cfedbc3432c266c1d5
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-30 18:59:37 +0200

    Alert API

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import { View, ActivityIndicator } from 'react-native';
+import { View, Alert, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <ActivityIndicator size={'large'} color={'red'} animating={true} />
+      <Button title="alert" onPress={() => Alert.alert('invalid data')} />
+      <Button
+        title="alert2"
+        onPress={() => Alert.alert('invalid data', 'DOB incorrect')}
+      />
+      <Button
+        title="alert3"
+        onPress={() =>
+          Alert.alert('invalid data', 'DOB incorrect', [
+            {
+              text: 'cancel',
+              onPress: () => console.log('cancel pressed'),
+            },
+            {
+              text: 'ok',
+              onPress: () => console.log('ok pressed'),
+            },
+          ])
+        }
+      />
     </View>
   );
 }
