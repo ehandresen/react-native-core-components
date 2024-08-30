09ed27bc6acff5a571a13916e422c05ca8408986
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-30 11:52:13 +0200

    ScrollView component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ImageBackground } from 'react-native';
+import { View, Text, Image, ImageBackground, ScrollView } from 'react-native';
 
 // using require (commonJS) is preferred when dealing with static assets like images in react-native
 const logoImg = require('./assets/adaptive-icon.png');
@@ -6,14 +6,24 @@ const logoImg = require('./assets/adaptive-icon.png');
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      <Image
-        source={{ uri: 'https://picsum.photos/300' }}
-        style={{ width: 300, height: 300 }}
-      />
-      {/* <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>image text</Text>
-      </ImageBackground> */}
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum. velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+          officia deserunt mollit anim id est laborum.
+        </Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </ScrollView>
     </View>
   );
 }
