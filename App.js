d46aeea13d4564cbbbefe694a7a9f39716fd8b99
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-30 13:44:23 +0200

    Button component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ImageBackground, ScrollView } from 'react-native';
+import { View, Button } from 'react-native';
 
 // using require (commonJS) is preferred when dealing with static assets like images in react-native
 const logoImg = require('./assets/adaptive-icon.png');
@@ -6,24 +6,11 @@ const logoImg = require('./assets/adaptive-icon.png');
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <ScrollView>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-          id est laborum. velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-          officia deserunt mollit anim id est laborum.
-        </Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </ScrollView>
+      <Button
+        title="press"
+        onPress={() => console.log('btn pressed')}
+        color="midnightblue"
+      />
     </View>
   );
 }
